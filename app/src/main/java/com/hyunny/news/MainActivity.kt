62cf00c03ea5bdb4d9d9a1719e757717a94eacf3
Seunghyun <shlee0207@gmail.com>
2023-03-01 23:05:32 +0900

    adding hilt & splash library

--- app/src/main/java/com/hyunny/news/MainActivity.kt
+++ app/src/main/java/com/hyunny/news/MainActivity.kt
@@ -10,11 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hyunny.news.ui.theme.TheNewsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+
         setContent {
             TheNewsTheme {
                 // A surface container using the 'background' color from the theme
