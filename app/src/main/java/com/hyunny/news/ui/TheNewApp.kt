3093482135b4a6921f60c213b399a6811c40f5c3
Seunghyun <shlee0207@gmail.com>
2023-03-16 23:36:59 +0900

    TheNewApp Screen

---
+++ app/src/main/java/com/hyunny/news/ui/TheNewApp.kt
@@ -0,0 +1,18 @@
+package com.hyunny.news.ui
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TheNewApp(
+
+) {
+
+    Scaffold(
+        bottomBar = {
+
+        }
+    ) { padding ->
+
+    }
+}
